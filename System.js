6c7e1921e734e159432c319c7a720c76f7171229
aadishah567 <77158154+aadishah567@users.noreply.github.com>
2021-06-09 00:00:32 +0530

    Add files via upload

---
+++ System.js
@@ -0,0 +1,23 @@
+class System{
+
+    constructor(){}
+
+    authenticate(actualCode,enteredCode){
+        textSize(50);
+        fill("black");
+        text(code,300,300)
+        //add code to return true if correct answer entered.
+
+        
+    }
+    authenticate(actualCode, enteredCode){
+
+        if(actualCode === enteredCode)
+
+            return true
+        else
+
+            return false
+
+       }
+}
